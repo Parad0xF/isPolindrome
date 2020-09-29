8dd7507fc4130797d8ec57b0075b5d4a86d7bab6
RaduE <radu.enachi@gmail.com>
2020-09-29 11:52:17 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,33 @@
+/* Radu Enachi 09/29/2020*/
+/* The promgram is checkin if the string is polindrome*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPolindrome(string s);
+
+int main() {
+	string s = "home";
+
+	cout << isPolindrome(s);
+
+	return 0;
+}
+
+bool isPolindrome(string s) {
+	int len = s.length();
+
+	if (s.length() == 1 || s.empty()) {
+		return true;
+	}
+	else if (s[0] == s[s.length() - 1]) {
+
+		
+		return isPolindrome(s.substr((0 + 1), (s.length() - 2)));
+	}
+	else
+		return false;
+
+}
